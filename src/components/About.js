3d9872ce81a709023c297e46c4691171e610b7c3
Favor Vincent <vfavorm77@gmail.com>
2024-04-25 17:20:05 +0300

    REACT props mini-project lab complete

---
+++ src/components/About.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function About(
+    {image = "https://via.placeholder.com/215Links to an external site.s", about})
+    {
+    return(
+    <aside>
+    <img src={image} alt="blog logo"/>
+    <p>{about}</p>
+    </aside>
+
+    )
+}
+
+export default About;
